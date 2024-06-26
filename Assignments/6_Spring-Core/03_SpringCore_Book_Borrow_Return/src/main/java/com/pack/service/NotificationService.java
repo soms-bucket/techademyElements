ea0e569e905ed428b47bf9b191a1d5c2807e6228
soms-bucket <87579641+soms-bucket@users.noreply.github.com>
2024-06-26 22:43:07 +0530

    Delete Assignments/6_Spring-Core directory

--- Assignments/6_Spring-Core/03_SpringCore_Book_Borrow_Return/src/main/java/com/pack/service/NotificationService.java
+++
@@ -1,8 +0,0 @@
-package com.pack.service;
-
-public class NotificationService {
-	public void notifyReturn(String bookTitle) {
-		System.out.println("Notification: The book '" + bookTitle + "' has been returned.");
-
-	}
-}
